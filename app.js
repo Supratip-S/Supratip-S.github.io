020ff7f292985bbd884f69df6e005f74364d2ce3
Supratip Santra <113226712+Supratip-S@users.noreply.github.com>
2026-02-23 22:08:19 +0530

    Prioritised AIML over Java

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ ScrollReveal().reveal('.bottom', {origin: 'bottom'})
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Java Backend Engineer', 'AI Systems Developer', 'Database Architect'],
+    strings: ['AI/ML Developer', 'Data Scientist', 'RAG Implementor', 'Backend AI Engineer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
